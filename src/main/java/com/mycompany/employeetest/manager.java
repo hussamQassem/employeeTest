1c449fb1e01792dda4237cfde075d7d2ae41545f
hussamQassem <hussamcacem@gmail.com>
2024-03-03 02:56:18 +0000

    class manager
    creating new class manager that has 2 variables and and set the class to extend the class employee

---
+++ src/main/java/com/mycompany/employeetest/manager.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.employeetest;
+
+/**
+ *
+ * @author hussa
+ */
+public class manager extends employee{
+    
+    private String username;
+    private String password;
+    // Constructor with default values
+    public manager() {
+       // super(); // to take the default constructor of the superclass (Employee)
+        this.username = "Gnomeo";
+        this.password = "smurf";
+    }
+
+    
+    
+    
+    
+}
